d569b435f9a36e1118fc663166867f524aff5c9a
Khusniddin Ruzimboev <115790500+datascientistn1@users.noreply.github.com>
2023-01-19 09:09:04 +0500

    Update settings.py

--- goodreads/settings.py
+++ goodreads/settings.py
@@ -142,7 +142,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'khusniddindatascientistn1@gmail.com'
+EMAIL_HOST_USER = '...@gmail.com'
 EMAIL_HOST_PASSWORD = 'xhjxnzgcugjcedzf'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
